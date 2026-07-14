bc5959808a2e4598d9377c3731ec3e6ac597915d
Preetham Gangadhikar <preethamgangadhikar@CES-T1QFYVQ91J.local>
2026-07-14 18:54:01 +1000

    Initial Version

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@
 
 export const SITE_TITLE = 'Aspect Infotech | Digital Workplace Solutions';
 export const SITE_DESCRIPTION =
-	'Secure digital workplace solutions for Australian small businesses using Microsoft 365, Intune, Entra ID and Copilot.';
+	'Aspect Infotech helps Australian small businesses design and implement secure digital workplaces using Microsoft 365, Intune, Entra ID and Copilot.';
 export const CONTACT_EMAIL = 'contact@aspectinfotech.com';
 export const BOOKING_LINK = 'https://calendly.com/pg-aspectinfotech/30min';
